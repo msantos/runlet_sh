72ab1b3a3422550919b29e00a6d78a0fa6c9c327
Michael Santos <michael.santos@gmail.com>
2020-03-06 10:19:58 -0500

    v1.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RunletSh.Mixfile do
   def project do
     [
       app: :runlet_sh,
-      version: "1.1.0",
+      version: "1.2.0",
       elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
