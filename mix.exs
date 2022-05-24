13e24feb20f44fe33d3b4d121820f43b1755a58e
Michael Santos <michael.santos@gmail.com>
2022-05-24 08:05:34 -0400

    Update deps

--- mix.exs
+++ mix.exs
@@ -23,30 +23,14 @@ defmodule RunletSh.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  # def application do
-  #  [applications: [:logger]]
-  # end
-
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:alcove, "~> 0.36.0"},
+      {:alcove, "~> 0.38.0"},
       {:prx, "~> 0.14.1"},
       {:runlet, github: "msantos/runlet"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
-      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
     ]
   end
 
